b79072d08aceb4936139ee1bc51979f47840adcc
Matthew Hendrey <matthew.hendrey@gmail.com>
2024-08-01 00:14:57 -0400

    Add in first text embedding model.
    Still need to setup perf_analyzer and validation.

---
+++ model_repository/embed_text/1/embed_text.py
@@ -0,0 +1,155 @@
+import asyncio
+import json
+from transformers import AutoProcessor
+
+import triton_python_backend_utils as pb_utils
+
+
+class TritonPythonModel:
+    """
+    Triton Inference Server deployment utilizing the python_backend for text embedding
+    models. Currently only SigLIP is supported.
+    """
+
+    def initialize(self, args):
+        """
+        Initialize embedding models' processors and load configuration parameters.
+
+        Parameters
+        ----------
+        args : dict
+            Command-line arguments for launching Triton Inference Server
+        """
+        self.model_config = model_config = json.loads(args["model_config"])
+        embedding_config = pb_utils.get_output_config_by_name(model_config, "EMBEDDING")
+        self.embedding_dtype = pb_utils.triton_string_to_numpy(
+            embedding_config["data_type"]
+        )
+
+        ## Load the different models needed for processing inputs
+        # Currently just the one model, but this is how to add additional ones
+        embed_models = json.loads(
+            model_config["parameters"]["embed_models"]["string_value"]
+        )
+        self.processors = {}
+        for embed_model, model_path in embed_models.items():
+            if embed_model == "siglip_text":
+                self.processors[embed_model] = AutoProcessor.from_pretrained(
+                    model_path,
+                    local_files=True,
+                )
+        ## Get additional parameters from the config.pbtxt file
+        # Specify the default embedding model. Can be overriden in request parameter
+        self.default_embed_model = model_config["parameters"]["default_embed_model"][
+            "string_value"
+        ]
+
+    def process_request(self, request, embed_model: str):
+        """
+        Process the input text request and prepare input_ids for embedding.
+
+        Parameters
+        ----------
+        request : pb_utils.InferenceRequest
+            Inference request containing the input text.
+        embed_model : str
+            Embedding model to use.
+
+        Returns
+        -------
+        np.ndarray
+            Tokenized text
+        """
+        try:
+            input_text_tt = pb_utils.get_input_tensor_by_name(request, "INPUT_TEXT")
+        except Exception as exc:
+            raise ValueError(f"Failed on getting input tensor from request: {exc}")
+
+        try:
+            input_text = [
+                b.decode("utf-8") for b in input_text_tt.as_numpy().reshape(-1)
+            ]
+        except Exception as exc:
+            raise ValueError(f"Failed on converting numpy to str request data: {exc}")
+
+        try:
+            if embed_model == "siglip_text":
+                input_ids_np = self.processors[embed_model](
+                    text=input_text, padding="max_length", return_tensors="pt"
+                )["input_ids"].numpy()
+        except Exception as exc:
+            raise ValueError(
+                f"Failed on {embed_model}'s Processor(text=input_text): {exc}"
+            )
+
+        # Shape = [batch_size, 64], where batch_size should be 1
+        return input_ids_np
+
+    async def execute(self, requests: list) -> list:
+        """
+        Execute a batch of embedding requests on provided texts.
+
+        Option Request Parameters
+        -------------------------
+        embed_model : str
+            Specify which embedding model to use.
+            If None, default_embed_model is used.
+
+        Parameters
+        ----------
+        requests : List[pb_utils.InferenceRequest]
+            List of inference requests each containing text to be embedded.
+
+        Returns
+        -------
+        List[pb_utils.InferenceResponse]
+            List of response objects with embedding results or error messages
+        """
+        logger = pb_utils.Logger
+        batch_size = len(requests)
+        logger.log_info(f"embed_text.execute received {batch_size} requests")
+        responses = [None] * batch_size
+        inference_response_awaits = []
+        valid_requests = []
+        for batch_id, request in enumerate(requests):
+            # Handle any request parameters
+            request_params = json.loads(request.parameters())
+            embed_model = request_params.get("embed_model", self.default_embed_model)
+
+            try:
+                input_ids_np = self.process_request(request, embed_model)
+                input_ids_tt = pb_utils.Tensor("INPUT_IDS", input_ids_np)
+            except Exception as exc:
+                response = pb_utils.InferenceResponse(
+                    error=pb_utils.TritonError(f"{exc}")
+                )
+                responses[batch_id] = response
+            else:
+                # Submit the request to embed_model
+                infer_model_request = pb_utils.InferenceRequest(
+                    model_name=embed_model,
+                    requested_output_names=["EMBEDDING"],
+                    inputs=[input_ids_tt],
+                )
+                # Perform asynchronous inference request
+                inference_response_awaits.append(infer_model_request.async_exec())
+                valid_requests.append(batch_id)
+
+        inference_responses = await asyncio.gather(*inference_response_awaits)
+        for model_response, batch_id in zip(inference_responses, valid_requests):
+            if model_response.has_error() and responses[batch_id] is None:
+                err_msg = (
+                    "Error embedding the text: " + f"{model_response.error().message()}"
+                )
+                response = pb_utils.InferenceResponse(
+                    error=pb_utils.TritonError(err_msg)
+                )
+                responses[batch_id] = response
+            else:
+                embedding_tt = pb_utils.get_output_tensor_by_name(
+                    model_response, "EMBEDDING"
+                )
+                response = pb_utils.InferenceResponse(output_tensors=[embedding_tt])
+                responses[batch_id] = response
+
+        return responses
---
+++ model_repository/siglip_text/1/siglip_text.py
@@ -0,0 +1,120 @@
+import json
+import torch
+from transformers import SiglipTextModel
+
+import triton_python_backend_utils as pb_utils
+
+
+class TritonPythonModel:
+    """
+    Triton Inference Server deployment utilizing the python_backend for SigLIP Text
+    model.
+    """
+
+    def initialize(self, args):
+        """
+        Initialize SiglipTextModel and load configuration parameters. Using
+        torch.compile() to speed up inference. The first few passes through the model
+        may be delayed while torch.compile() does its magic.
+
+        Parameters
+        ----------
+        args : dict
+            Command-line arguments for launching Triton Inference Server
+        """
+        self.model_config = model_config = json.loads(args["model_config"])
+        embedding_config = pb_utils.get_output_config_by_name(model_config, "EMBEDDING")
+        self.embedding_dtype = pb_utils.triton_string_to_numpy(
+            embedding_config["data_type"]
+        )
+        model_path = model_config["parameters"]["model_path"]["string_value"]
+
+        # Use the GPU if available, otherwise use the CPU
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+            self.torch_dtype = torch.float16
+        else:
+            self.device = torch.device("cpu")
+            self.torch_dtype = torch.float32  # CPUs can't handle float16
+
+        self.model = SiglipTextModel.from_pretrained(
+            model_path,
+            device_map="auto",
+            torch_dtype=self.torch_dtype,
+            local_files_only=True,
+            use_safetensors=True,
+        )
+        # If on a GPU, use torch.compile to improve throughput
+        if torch.cuda.is_available():
+            self.model = torch.compile(self.model, dynamic=True)
+
+    def execute(self, requests: list) -> list:
+        """
+        Execute a batch of embedding requests on provided texts that have already
+        been converted to `input_ids`. When using
+        `processor(text=prompts, padding='max_length')`, it will automatically pad
+        with `1`.
+
+        Shape = (64,), dtype=np.int64
+
+        Parameters
+        ----------
+        requests : List[pb_utils.InferenceRequest]
+            List of inference requests each containing an image to be embedded.
+
+        Returns
+        -------
+        List[pb_utils.InferenceResponse]
+            List of response objects with embedding results or error messages
+        """
+        logger = pb_utils.Logger
+        batch_size = len(requests)
+        logger.log_info(f"siglip_text.execute received {batch_size} requests")
+        responses = [None] * batch_size
+        batch_input_ids = []
+        valid_requests = []
+        for batch_id, request in enumerate(requests):
+            try:
+                input_ids = torch.from_numpy(
+                    pb_utils.get_input_tensor_by_name(
+                        request,
+                        "INPUT_IDS",
+                    ).as_numpy()
+                )
+            except Exception as exc:
+                response = pb_utils.InferenceResponse(
+                    error=pb_utils.TritonError(f"{exc}")
+                )
+                responses[batch_id] = response
+            else:
+                batch_input_ids.append(input_ids)
+                valid_requests.append(batch_id)
+
+        # Create batch to be processed shape=[len(valid_requests), 64]
+        batch_input_ids = torch.cat(batch_input_ids, dim=0).to(self.device)
+        try:
+            with torch.no_grad():
+                text_embedding_np = (
+                    self.model(input_ids=batch_input_ids)["pooler_output"]
+                    .cpu()
+                    .type(torch.float32)
+                    .numpy()
+                )
+        except Exception as exc:
+            # Problem embedding the whole batch. They all failed
+            for i in valid_requests:
+                response = pb_utils.InferenceResponse(
+                    error=pb_utils.TritonError(
+                        "siglip_text threw error embedding the batch. Check your "
+                        + "input text and/or try again"
+                    )
+                )
+                responses[i] = response
+            return responses
+
+        for i, embedding in zip(valid_requests, text_embedding_np):
+            embedding_tt = pb_utils.Tensor("EMBEDDING", embedding.reshape(1, -1))
+            response = pb_utils.InferenceResponse(output_tensors=[embedding_tt])
+            responses[i] = response
+
+        return responses
